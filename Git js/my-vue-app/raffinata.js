278f20bdb4030ad1d373ef5bd00c76f1b1152fd0
ImFormal <thubert.maxime.pro@gmail.com>
2024-11-12 11:46:57 +0100

    Ajout des exercices JS vite

---
+++ Git js/my-vue-app/raffinata.js
@@ -0,0 +1,15 @@
+let nomPizzeria = '"La Pizzeria Raffinata"';
+let taille = "XL";
+let nomPizza = "Pizza Texan";
+let base = "Crème"; 
+let heureCommande = "21:04:03";
+let heureLivraison = "21:45:00";
+let supplement = "Aucun";
+let elementAEnlever = "Olives";
+let nomClient = "Martin Martin";
+let adresseClient = "3 rue des martins, Martinville";
+let prix = "14€";
+let blague = 'Qu\'est-ce qu\'une pizza dit à une autre pizza qui lui fait de l\'ombre\n?"Arrête de me trancher la route!"'
+
+let SumUpOrderPhrase = "[" + heureCommande + "]\nVous venez de commander une " + nomPizza + " " + taille + " base " + base + " à " + prix + " avec en supplément : " + supplement + " et sans : " + elementAEnlever + ".\nVous serez livré à " + heureLivraison + " à l'adresse : " + adresseClient + " au nom de " + nomClient + "\nPetite blague : " + blague + "\nMerci d'avoir commandé chez " + nomPizzeria;
+console.log(SumUpOrderPhrase);
