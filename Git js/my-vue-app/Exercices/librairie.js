c2b4855dd19c105bbdb9419fd8850173a95c4cf3
ImFormal <thubert.maxime.pro@gmail.com>
2024-11-13 14:11:05 +0100

    Ajout exo librairie

---
+++ Git js/my-vue-app/Exercices/librairie.js
@@ -0,0 +1,9 @@
+VANTA.CLOUDS({
+    el: '#app',
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: window.screen.height/2,
+    minWidth: window.screen.width,
+    speed: 1.90
+  })
