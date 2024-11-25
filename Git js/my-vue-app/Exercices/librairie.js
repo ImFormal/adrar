3fb1b218f9ba3e78466d8cc7d9af6d5accf9533f
ImFormal <thubert.maxime.pro@gmail.com>
2024-11-25 15:13:10 +0100

    Ajout attackPlayer & attackMonster

--- Git js/my-vue-app/Exercices/librairie.js
+++
@@ -1,9 +0,0 @@
-VANTA.CLOUDS({
-    el: '#app',
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: window.screen.height/2,
-    minWidth: window.screen.width,
-    speed: 1.90
-  })
--- Git js/my-vue-app/Exercices/progressBar.js
+++
@@ -1,7 +0,0 @@
-const laBar = document.querySelector(".bar");
-
-document.addEventListener("scroll", function(){
-  const scrollMax = document.body.scrollHeight - window.innerHeight;
-  const onEstOu = (window.scrollY / scrollMax) * 100;
-  laBar.style.width = onEstOu + "%";
-});
